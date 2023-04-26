6323b81a0cdafa1da0c518fb25a2474d0e5322c7
AlicePom <pomeislova@seznam.cz>
2023-04-26 22:24:09 +0200

    Úkol 4 - s bonusem č. 1 (odstranění mezer)

--- C4_domaci_ukol.py
+++ C4_domaci_ukol.py
@@ -1,6 +1,7 @@
 import math
 
 tel_cislo = input("Zadejte prosím telefonní číslo, na které chcete zprávu zaslat: ")
+tel_cislo = tel_cislo.replace(" ", "")
 
 def overeni(tel_cislo):
     if len(tel_cislo) == 13 and tel_cislo[0:4] == "+420":
@@ -15,7 +16,7 @@ if overeni(tel_cislo) == True:
     zprava = input("Zadejte prosím zprávu, kterou chcete odeslat: ")
     cena_za_1_SMS = 3
     def cena_zpravy(zprava):
-        delka = len(zprava)
+        delka = int(len(zprava))
         pocet_SMS = math.ceil(delka / 180)
         cena = cena_za_1_SMS * pocet_SMS
         return cena
