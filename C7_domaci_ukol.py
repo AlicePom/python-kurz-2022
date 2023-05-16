34408a513bd37c312d7ca59269c7bbfe98ccc241
AlicePom <pomeislova@seznam.cz>
2023-05-16 15:14:29 +0200

    Úkol č. 7 - oprava

--- C7_domaci_ukol.py
+++ C7_domaci_ukol.py
@@ -7,52 +7,46 @@ class Auto:
     
     def pujc_auto(self):
         if self.dostupne:
-            text = f"Potvrzuji zapůjčení vozidla."
+            text = "Potvrzuji zapůjčení vozidla."
+            self.dostupne = False
         else:
-            text = f"Vozidlo není k dispozici."
+            text = "Vozidlo není k dispozici."
         return text
 
     def get_info(self):
         return f"Automobil {self.typ_vozidla}, registrační značka {self.registracni_znacka}."
     
-    def vrat_auto(self, stav_tachometru_vraceni, pocet_dni_uzivani):
-        self.stav_tachometru_vraceni = stav_tachometru_vraceni
-        # self.pocet_dni_uzivani = pocet_dni_uzivani
+    def vrat_auto(self, najete_km, pocet_dni_uzivani):
+        self.najete_km = najete_km
         self.volne = True
-        """ 
-        # Funkce mi nefunguje, viz níže line 51, neumím ji zavolat
 
-        if pocet_dni_uzivani < 7:
-            cena = f"Cena za 1 den vypůjčení je 400 Kč. Prosím, zaplaťte {self.pocet_dni_uzivani * 400} Kč."
-        elif pocet_dni_uzivani >= 7:
-            cena = f"Cena za 1 den vypůjčení je 300 Kč. Prosím, zaplaťte {self.pocet_dni_uzivani * 300} Kč."
-        return cena 
-        """
-
-Peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
-Škoda = Auto("1P3 4747", "Škoda Octavia", 41253)
+peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
+skoda = Auto("1P3 4747", "Škoda Octavia", 41253)
 
 dotaz_na_auto = input("Zadejte prosím typ vozu, který si chcete vypůjčit (Peugeot/Škoda): ")
 
 if dotaz_na_auto == "Peugeot":
-    print(Peugeot.get_info())
-    print(Peugeot.pujc_auto())
-    Peugeot.dostupne = False
-    # self.dostupne = False
+    print(peugeot.get_info())
+    print(peugeot.pujc_auto())
+    print(peugeot.pujc_auto())
     
 if dotaz_na_auto == "Škoda":
-    print(Škoda.get_info())
-    print(Škoda.pujc_auto())
-    Škoda.dostupne = False
-    # self.dostupne = False
+    print(skoda.get_info())
+    print(skoda.pujc_auto())
+    print(skoda.pujc_auto())
 
 pocet_dni_uzivani = int(input("Kolik dní jste měl/a auto vypůjčené? "))
-# "print(pocet_dni_uzivani.vrat_auto())" nefunguje, funkci neumím zavolat
 
-# Toto (asi ne požadované) řešení mi původně (nejak) fungovalo, pak jsem se snažila řešení modifikovat (neuložila jsem si původní) a nyní nefunguje, hlásí mi to ValueError: invalid literal for int() with base 10: ''
 if pocet_dni_uzivani < 7:
     print(f"Cena za 1 den vypůjčení je 400 Kč. Prosím, zaplaťte {pocet_dni_uzivani * 400} Kč.")
 if pocet_dni_uzivani >= 7:
     print(f"Cena za 1 den vypůjčení je 300 Kč. Prosím, zaplaťte {pocet_dni_uzivani * 300} Kč.") 
 
+najete_km = int(input("Jaky je stav tachometru pri vraceni? "))
 
+if dotaz_na_auto == "Peugeot":
+    peugeot.vrat_auto(najete_km, pocet_dni_uzivani)
+    print(f"Automobil Peugeot má nyní najeto {najete_km} km.")
+if dotaz_na_auto == "Škoda":
+    skoda.vrat_auto(najete_km, pocet_dni_uzivani)
+    print(f"Automobil Škoda má nyní najeto {najete_km} km.")
